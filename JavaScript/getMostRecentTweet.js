951c17a96e9743b162e6a6aba25bd5936eff4a6c
Daniel <danielfcarmichael@gmail.com>
2021-12-16 18:36:09 -0500

    Cleaning up files

--- JavaScript/getMostRecentTweet.js
+++ JavaScript/getMostRecentTweet.js
@@ -1,3 +1,17 @@
+const fs = require("fs")
+
+/*
+// open file of pulled data and split it by line
+try {
+  const file = fs.readFileSync('ElonData.txt', 'utf8')
+  var data = file
+}
+catch (err) {
+  console.error(err)
+}
+*/
+
+
 // Search for Tweets within the past seven days
 // https://developer.twitter.com/en/docs/twitter-api/tweets/search/quick-start/recent-search
 
@@ -12,43 +26,51 @@ const endpointUrl = "https://api.twitter.com/2/tweets/search/recent";
 
 var mostRecentId = '';
 
-async function getRequest() {
-
-    // Edit query parameters below
-    // specify a search query, and any additional fields that are required
-    // by default, only the Tweet ID and text fields are returned
-    const params = {
-        'query': 'from:elonmusk -is:retweet',
-        'tweet.fields': 'author_id',
-        'since_id': '1471555887387058184'
-    }
-
-    const res = await needle('get', endpointUrl, params, {
-        headers: {
-            "User-Agent": "v2RecentSearchJS",
-            "authorization": `Bearer ${token}`
+function getMostRecentId() {
+    async function getRequest() {
+    
+        // Edit query parameters below
+        // specify a search query, and any additional fields that are required
+        // by default, only the Tweet ID and text fields are returned
+        const params = {
+            'query': 'from:elonmusk -is:retweet',
+            'tweet.fields': 'author_id',
+            //'since_id': '1471555887387058184'
         }
-    })
-
-    if (res.body) {
-        return res.body;
-    } else {
-        throw new Error('Unsuccessful request');
+    
+        const res = await needle('get', endpointUrl, params, {
+            headers: {
+                "User-Agent": "v2RecentSearchJS",
+                "authorization": `Bearer ${token}`
+            }
+        })
+    
+        if (res.body) {
+            return res.body;
+        } else {
+            throw new Error('Unsuccessful request');
+        }
     }
+    
+    (async () => {
+    
+        try {
+            // Make request
+            const response = await getRequest();
+            return response.data[9].id
+    
+        } catch (e) {
+            console.log(e);
+            process.exit(-1);
+        }
+        process.exit();
+    })().then(data => (mostRecentId = data));
+    //setTimeout(() => {console.log(mostRecentId)}, 1000)
 }
 
-(async () => {
+//getMostRecentId();
 
-    try {
-        // Make request
-        const response = await getRequest();
-        console.dir(response, {
-            depth: null
-        });
-
-    } catch (e) {
-        console.log(e);
-        process.exit(-1);
-    }
-    process.exit();
-})();
+fs.writeFile('recentid.txt', getMostRecentId(), (err) => {
+    // In case of a error throw err.
+    if (err) throw err;
+  })
