bb986bba6c83cb7f3ee9e3667828b4186cb38dec
Daniel <danielfcarmichael@gmail.com>
2021-12-16 17:44:21 -0500

    More accurate file names and beginning automation

---
+++ JavaScript/getMostRecentTweet.js
@@ -0,0 +1,54 @@
+// Search for Tweets within the past seven days
+// https://developer.twitter.com/en/docs/twitter-api/tweets/search/quick-start/recent-search
+
+const needle = require('needle');
+
+// The code below sets the bearer token from your environment variables
+// To set environment variables on macOS or Linux, run the export command below from the terminal:
+// export BEARER_TOKEN='YOUR-TOKEN'
+const token = 'AAAAAAAAAAAAAAAAAAAAAHVSWwEAAAAAOn%2FFskPge8QQHXr3bnUS8BvpFQY%3DIL7o0vYJHC3sFQtBHnJqkSVwMoiEBtmJdFRZnrNyuNf0y4RUfF';
+
+const endpointUrl = "https://api.twitter.com/2/tweets/search/recent";
+
+var mostRecentId = '';
+
+async function getRequest() {
+
+    // Edit query parameters below
+    // specify a search query, and any additional fields that are required
+    // by default, only the Tweet ID and text fields are returned
+    const params = {
+        'query': 'from:elonmusk -is:retweet',
+        'tweet.fields': 'author_id',
+        'since_id': '1471555887387058184'
+    }
+
+    const res = await needle('get', endpointUrl, params, {
+        headers: {
+            "User-Agent": "v2RecentSearchJS",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (res.body) {
+        return res.body;
+    } else {
+        throw new Error('Unsuccessful request');
+    }
+}
+
+(async () => {
+
+    try {
+        // Make request
+        const response = await getRequest();
+        console.dir(response, {
+            depth: null
+        });
+
+    } catch (e) {
+        console.log(e);
+        process.exit(-1);
+    }
+    process.exit();
+})();
--- JavaScript/getPastData.js
+++ JavaScript/getPastData.js
@@ -12,6 +12,7 @@ const url = `https://api.twitter.com/2/users/${userId}/tweets`;
 // The code below sets the bearer token from your environment variables
 // To set environment variables on macOS or Linux, run the export command below from the terminal:
 // export BEARER_TOKEN='YOUR-TOKEN'
+// bearerToken = process.env.BEARER_TOKEN
 const bearerToken = 'AAAAAAAAAAAAAAAAAAAAAHVSWwEAAAAAOn%2FFskPge8QQHXr3bnUS8BvpFQY%3DIL7o0vYJHC3sFQtBHnJqkSVwMoiEBtmJdFRZnrNyuNf0y4RUfF';
 
 const getUserTweets = async () => {
