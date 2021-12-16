44508cd47b3462b3dc14e43914315604e923281f
Daniel <danielfcarmichael@gmail.com>
2021-12-16 01:35:39 -0500

    Added best guess functionality for current time

---
+++ JavaScript/getTime.js
@@ -0,0 +1,17 @@
+function calculate() {
+    const now = new Date();
+    var hours = now.getUTCHours()
+    //hours = parseInt(hours)
+    var response = ""
+    if (parseInt(hours) >= 2 && parseInt(hours) <= 4) {
+        response = "is probably pooping!!!"
+    }
+    else if (parseInt(hours) >= 1 && parseInt(hours) < 2) {
+        response = "might poop here pretty soon..."
+    }
+    else {
+        response = "isn't pooping for a little while :^("
+    }
+
+    document.getElementById("isHePooping").innerHTML = "Elon Musk =" + response;
+}
